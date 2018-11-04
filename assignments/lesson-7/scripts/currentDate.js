0aa195670ad518b2cb31134661f8069922e38dfd
germandemiandemarco <germandemiandemarco@gmail.com>
2018-11-04 12:23:43 -0300

    .

---
+++ assignments/lesson-7/scripts/currentDate.js
@@ -0,0 +1,35 @@
+
+
+function currentDate(){
+
+var month = new Array();
+var weekday = new Array(7);
+
+weekday[0] = "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
+month[0] = "January";
+month[1] = "February";
+month[2] = "March";
+month[3] = "April";
+month[4] = "May";
+month[5] = "June";
+month[6] = "July";
+month[7] = "August";
+month[8] = "September";
+month[9] = "October";
+month[10] = "November";
+month[11] = "December";
+
+var d = new Date();
+var n = month[d.getMonth()];
+var m = weekday[d.getDay()];
+
+document.getElementById("outPut").innerHTML = m + " " + d.getDate() +", " + n + " " + d.getFullYear();
+
+
+}
