a907ed96df1714a0fba09d35e0dde0a918c7c2b9
Germandemiandemarco <43485126+Germandemiandemarco@users.noreply.github.com>
2019-02-24 11:24:15 -0300

    Update windChill.js

--- assignments/lesson-7/scripts/windChill.js
+++ assignments/lesson-7/scripts/windChill.js
@@ -1,14 +1,10 @@
 
-   var tempL = parseFloat(document.getElementById('low').value);
-   var tempH = parseFloat(document.getElementById('high').value);
-   var temperature = parseFloat((tempL + tempH) / 2);
-   
-
-var  speed = parseFloat(document.getElementById('windSpeed').value);
-
-   var f =  35.74 + 0.6215 * temperature - 35.75 * windSpeed+ 0.4275 * temperature * windSpeed;
-   
-   document.getElementById('outputDiv').innerHTML = f;
   
+   var temperature = 76;
+   var windSpeed = 5;
+   var round = Math.round(2);
 
-   
+   var f =  35.74 + 0.6215 * temperature - 35.75 * windSpeed + 0.4275 * temperature * windSpeed;
+
+   document.getElementById('conversion').innerHTML = Math.round(f, 2);
+  
