e91e173433cf2b105232113c1415e32146b0f681
Germandemiandemarco <43485126+Germandemiandemarco@users.noreply.github.com>
2019-03-27 11:44:35 -0300

    Update weatherApi.js

--- assignments/weatherSite/weatherApi.js
+++ assignments/weatherSite/weatherApi.js
@@ -35,29 +35,3 @@ forecastRequest.onload =  function () {
         }
 }
 
-
-let weatherRequest = new XMLHttpRequest();
-
-var apiURLstring = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&APPID=be4b279b62e97c1113c247c06009cdee&units=imperial';
-
-weatherRequest.open('GET', apiURLstring, true);
-
-weatherRequest.send();
-
-weatherRequest.onload =  function () {
-    let weatherData = JSON.parse(weatherRequest.responseText);
-
-    var feature = weatherData['main'];
-    var weather = weatherData['weather'];
-    var wind = weatherData['wind'];
-
-    document.getElementById('tempOutput').innerHTML = feature.temp + "&deg; F";
-    document.getElementById('currentOutput').innerHTML = weather[0].main;
-    document.getElementById('speedOutput').innerHTML = wind.speed + " mph";
-    document.getElementById('humidOutput').innerHTML = feature.humidity + "%";
-
-    var windChill = getWindChill(feature.temp, wind.speed);
-
-    document.getElementById("windOutput").innerHTML = (Math.round(windChill * 10) / 10) + "&deg; F";
-
-}
